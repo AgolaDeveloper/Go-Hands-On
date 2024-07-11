345b2c1b7f1bc5c1b530a0d305f7e19930a54f6e
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-11 14:43:43 +0300

    Pass Data Struct (phonebook) as argument to the method

---
+++ Maps/Mini-Projects/Phonebook-App/deleteContact.go
@@ -0,0 +1,10 @@
+// this deletes a contact from our Phonebook [MAP Data Structure]
+package main
+
+//this func deletes a contact from the phonebook
+//must check if the contact is present though
+
+//this func takes phoneBook data struct as a parameter and deletes a contact from it
+func deleteContact(phoneBook func() map[string]string) {
+
+}
