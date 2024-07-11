d121ab0b694a1646e961e108627575cf0e83920b
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-11 14:30:02 +0300

    Call addContact method with phonebook method passed as argument

---
+++ Maps/Mini-Projects/Phonebook-App/main.go
@@ -0,0 +1,7 @@
+// This program creates a simple phonebook application where you can add, delete, and search for contacts using a map.
+package main
+
+func main() {
+	//add Contact
+	addContact(phonebook)
+}
