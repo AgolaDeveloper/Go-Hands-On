fe02d6b679e3e69100a0b5277bd134832b9e4d59
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-11 21:45:44 +0300

    Add for-loop in main.go file

--- Maps/Mini-Projects/Phonebook-App/main.go
+++ Maps/Mini-Projects/Phonebook-App/main.go
@@ -7,32 +7,49 @@ func main() {
 	phonebook := Phonebook()
 	//your phonebook
 	DisplayPhonebook(phonebook)
+	for {
+		var choice int
+		fmt.Println("1. PHONEBOOK")
+		fmt.Println("2. ADD Contact")
+		fmt.Println("3. DELETE Contact")
+		fmt.Println("4. SEARCH Contact")
 
-	var choice int
-	fmt.Println("1. PHONEBOOK")
-	fmt.Println("2. ADD Contact")
-	fmt.Println("3. DELETE Contact")
-	fmt.Println("4. SEARCH Contact")
+		fmt.Scan(&choice)
 
-	fmt.Scan(&choice)
+		switch choice {
+		case 1:
+			//your phonebook
+			DisplayPhonebook(phonebook)
+		case 2:
+			//add Contact... call addContact with phonebook passed as argument
+			//... it'll write to phonebook['s Map Data Struct]
+			AddContact(phonebook)
+		case 3:
+			//Delete Contact... call addContact with phonebook passed as argument
+			//... it'll delete an existing contact from the phonebook['s Map Data Struct]
+			DeleteContact(phonebook)
+		case 4:
+			//Delete Contact... call addContact with phonebook passed as argument
+			//... it'll delete an existing contact from the phonebook['s Map Data Struct]
+			SearchContact(phonebook)
 
-	switch choice {
-	case 1:
-		//your phonebook
-		DisplayPhonebook(phonebook)
-	case 2:
-		//add Contact... call addContact with phonebook passed as argument
-		//... it'll write to phonebook['s Map Data Struct]
-		AddContact(phonebook)
-	case 3:
-		//Delete Contact... call addContact with phonebook passed as argument
-		//... it'll delete an existing contact from the phonebook['s Map Data Struct]
-		DeleteContact(phonebook)
-	case 4:
-		//Delete Contact... call addContact with phonebook passed as argument
-		//... it'll delete an existing contact from the phonebook['s Map Data Struct]
-		SearchContact(phonebook)
+		}
 
+		fmt.Println("*****************************************************")
+
+	retry:
+		var cont string
+		fmt.Println("Continue-(y/n) >>: ")
+		fmt.Scan(&cont)
+
+		if cont == "y" || cont == "Y" {
+			continue
+		} else if cont == "n" || cont == "N" {
+			break
+		} else {
+			fmt.Println("Invalid Choice...retry")
+			goto retry
+		}
 	}
 
 }
