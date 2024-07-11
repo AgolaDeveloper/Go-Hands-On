a83b3c9130a87fb24e2db97ac4608a3f65e0c642
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-11 14:37:30 +0300

    Add deleteContact file

--- Maps/Mini-Projects/Phonebook-App/phoneBook.go
+++ Maps/Mini-Projects/Phonebook-App/phoneBook.go
@@ -2,6 +2,8 @@
 package main
 
 //func that creates creates the struct
+
+//it takes nothing as a parameter since it does use any imporetd return values
 func phonebook() map[string]string {
 	//creating an empty map ready to store contact details
 	phoneBook := make(map[string]string)
