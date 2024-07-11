0b29c1d72d2d7661fbf1d45c1f89c9bbb55ac952
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-11 16:53:15 +0300

    Fix: check if phonebook is empty or not

--- Maps/Mini-Projects/Phonebook-App/searchContact.go
+++ Maps/Mini-Projects/Phonebook-App/searchContact.go
@@ -6,24 +6,31 @@ import "fmt"
 //the func ranges through phonebook and displays contact details
 func searchContact(phonebook func() map[string]string) {
 	phoneBookMap := phonebook()
-	var cont string
 
-	fmt.Println("Search>>\n >>: ")
-	fmt.Scan(&cont)
-	//first check if the search contact exist
-	_, ok := phoneBookMap[cont]
-
-	if !ok {
-		//if it doesn't exist
-		fmt.Printf("\nError... %v Not in Phonebook\n", cont)
+	if phoneBookMap == nil {
+		//if it's nill/empty >>then deletion impossible
+		fmt.Println("PhoneBook Empty")
 	} else {
-		//else... if it exists then show the contactName and Number
-		for key, value := range phoneBookMap {
-			//print key and value where key equals cont
-			if key == cont {
-				fmt.Printf("\n Name: %v\n Contact Number: %v\n", key, value)
+		var cont string
 
+		fmt.Println("Search>>\n >>: ")
+		fmt.Scan(&cont)
+		//first check if the search contact exist
+		_, ok := phoneBookMap[cont]
+
+		if !ok {
+			//if it doesn't exist
+			fmt.Printf("\nError... %v Not in Phonebook\n", cont)
+		} else {
+			//else... if it exists then show the contactName and Number
+			for key, value := range phoneBookMap {
+				//print key and value where key equals cont
+				if key == cont {
+					fmt.Printf("\n Name: %v\n Contact Number: %v\n", key, value)
+
+				}
 			}
+
 		}
 
 	}
