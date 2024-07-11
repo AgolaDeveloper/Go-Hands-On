85a048c113bc8c0d63aab0b0c6cba5488ff5cdfd
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-11 14:20:04 +0300

    Define function that creates the App's phonebook data struct (map)

---
+++ Maps/Mini-Projects/Phonebook-App/phoneBook.go
@@ -0,0 +1,11 @@
+// this is the data structure that stores all our contact details [for now]
+package main
+
+//func that creates creates the struct
+func phonebook() map[string]string {
+	//creating an empty map ready to store contact details
+	phoneBook := make(map[string]string)
+
+	//in case the func is called it returns the phoneBook Struct [map]
+	return phoneBook
+}
