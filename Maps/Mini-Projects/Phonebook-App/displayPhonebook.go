7a910ca80cdc356a6b19ee22c842aaf18e9cea5b
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-11 17:08:21 +0300

    Define displayPhonebook function

---
+++ Maps/Mini-Projects/Phonebook-App/displayPhonebook.go
@@ -0,0 +1,33 @@
+// this is the phonebook; displays everything in our phonebook Map struct
+package main
+
+import "fmt"
+
+//func checks if phonebook is empty then display all the contact
+//it takes phonebook Map func as an argument
+
+func displayPhonebook() {
+
+	phoneBookMap := phonebook()
+
+	//first checks whether phonebook is empty or not
+	if phoneBookMap == nil {
+
+		//if it's nill/empty >>then print empty
+		fmt.Println("PhoneBook Empty")
+	} else {
+		//else, phonebook get displayed
+		//by ranging through the entire phonebook Map
+
+		fmt.Println("Your PhoneBook:")
+		fmt.Println("*______________________________________________*")
+		fmt.Println()
+		for key, value := range phoneBookMap {
+			//we only display Names [keys] to the user to choose
+			fmt.Printf("***\n%v:\n%v\n\n", key, value)
+		}
+		fmt.Println()
+		fmt.Println("*______________________________________________*")
+
+	}
+}
