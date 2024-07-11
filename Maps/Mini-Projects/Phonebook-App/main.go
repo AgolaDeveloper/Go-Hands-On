7b4c9d52e40d85b9c82df3b9ee73337e4b6a821e
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-11 17:22:14 +0300

    Add choices for users

--- Maps/Mini-Projects/Phonebook-App/main.go
+++ Maps/Mini-Projects/Phonebook-App/main.go
@@ -1,9 +1,19 @@
 // This program creates a simple phonebook application where you can add, delete, and search for contacts using a map.
 package main
 
+import "fmt"
+
 func main() {
 	//your phonebook
 	displayPhonebook(phonebook)
+
+	var choice string
+	fmt.Println("1. PHONEBOOK")
+	fmt.Println("2. ADD Contact")
+	fmt.Println("3. DELETE Contact")
+	fmt.Println("4. SEARCH Contact")
+
+	fmt.Scan(&choice)
 
 	//add Contact... call addContact with phonebook passed as argument
 	//... it'll write to phonebook['s Map Data Struct]
