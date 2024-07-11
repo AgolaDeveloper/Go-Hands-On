bbdf63bfa5f83b55f6df7a6b35c457b41086439a
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-11 17:27:51 +0300

    Add switch control for user choices to navigate the App

--- Maps/Mini-Projects/Phonebook-App/main.go
+++ Maps/Mini-Projects/Phonebook-App/main.go
@@ -7,7 +7,7 @@ func main() {
 	//your phonebook
 	displayPhonebook(phonebook)
 
-	var choice string
+	var choice int
 	fmt.Println("1. PHONEBOOK")
 	fmt.Println("2. ADD Contact")
 	fmt.Println("3. DELETE Contact")
@@ -15,16 +15,23 @@ func main() {
 
 	fmt.Scan(&choice)
 
-	//add Contact... call addContact with phonebook passed as argument
-	//... it'll write to phonebook['s Map Data Struct]
-	addContact(phonebook)
+	switch choice {
+	case 1:
+		//your phonebook
+		displayPhonebook(phonebook)
+	case 2:
+		//add Contact... call addContact with phonebook passed as argument
+		//... it'll write to phonebook['s Map Data Struct]
+		addContact(phonebook)
+	case 3:
+		//Delete Contact... call addContact with phonebook passed as argument
+		//... it'll delete an existing contact from the phonebook['s Map Data Struct]
+		deleteContact(phonebook)
+	case 4:
+		//Delete Contact... call addContact with phonebook passed as argument
+		//... it'll delete an existing contact from the phonebook['s Map Data Struct]
+		searchContact(phonebook)
 
-	//Delete Contact... call addContact with phonebook passed as argument
-	//... it'll delete an existing contact from the phonebook['s Map Data Struct]
-	deleteContact(phonebook)
-
-	//Delete Contact... call addContact with phonebook passed as argument
-	//... it'll delete an existing contact from the phonebook['s Map Data Struct]
-	searchContact(phonebook)
+	}
 
 }
