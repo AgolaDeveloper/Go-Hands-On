47634abb27fb3ad09db8339ae4e939786a9a8e4a
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-14 00:52:05 +0300

    Modify displaySlices.go; define it's function

--- ArraySlices/Practice/RemoveDuplicates/displaySlices.go
+++ ArraySlices/Practice/RemoveDuplicates/displaySlices.go
@@ -1,2 +1,23 @@
-// this program file dispplays original slice[with duplicates] and sorted one [without duplicates]
+// this program file displays original slice[with duplicates] and sorted one [without duplicates]
 package main
+
+import "fmt"
+
+//it takes two parameters
+//the original slice with duplicates [return value of popSlice]
+//...& sorted slice without duplicates [return value of removeDuplicate]
+func displaySlices(originalSlice, sortedSlice []int) {
+	//print both slices
+
+	fmt.Println("Original Slice [With Duplicates]: ", originalSlice)
+	for index, value := range originalSlice {
+		fmt.Printf("index %v: %v\n", index, value)
+	}
+	fmt.Println("____________________________________________________")
+
+	fmt.Println("New Slice [Without Duplicates]: ", sortedSlice)
+	for index, value := range sortedSlice {
+		fmt.Printf("index %v: %v\n", index, value)
+
+	}
+}
