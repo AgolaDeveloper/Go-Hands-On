d6f6092766d5069f880b143b9688686a1e2342fd
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-13 22:33:15 +0300

    Add main and popSlice files; then define slice function

---
+++ ArraySlices/Practice/RemoveDuplicates/main.go
@@ -0,0 +1,7 @@
+// this is the main program file
+// it's the entry point to our program
+package main
+
+func main() {
+
+}
---
+++ ArraySlices/Practice/RemoveDuplicates/popSlice.go
@@ -0,0 +1,9 @@
+// this file acts as our data structure for our practice program
+// it creates a struct that we'll use to store our basic data [types] elements
+package main
+
+func slice() []int {
+	//this function creates our basic data struct[slice in this case]
+	ourSlice := make([]int, 0)
+	return ourSlice
+}
