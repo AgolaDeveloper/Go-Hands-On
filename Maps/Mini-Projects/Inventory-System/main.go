e315699be69e669a9b42245110f6d4d4dba24e7c
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-15 18:17:11 +0300

    Adjust main.go; fix user choice

--- Maps/Mini-Projects/Inventory-System/main.go
+++ Maps/Mini-Projects/Inventory-System/main.go
@@ -2,15 +2,28 @@ package main
 
 import "fmt"
 
+//welcome func to the inventory management system
+func welkam() {
+	fmt.Println("INVENTORY ")
+}
+
 //entry point of the Application
 func main() {
 	//first, initialize inventory's method
 	ourStruct := ourStruct()
+
+	var userChoice int
+
+	fmt.Println("1. Stock Up")
+	fmt.Println("2. Check Stock")
+
+	fmt.Scan(&userChoice)
 
 	//add item to the inventory... passing inventory's func as argument
 	fmt.Println("Stock Up! Add Item")
 	AddItem(ourStruct)
 
 	//check/display available items in the inventory...taking inventory's func as argument
+	fmt.Println("Check ")
 	Items(ourStruct)
 }
