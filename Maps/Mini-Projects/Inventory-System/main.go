fb5a299f890e70776d51db8060f35c51d0199178
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-15 18:48:26 +0300

    Modify main.go file; controluser choice with SWITCH

--- Maps/Mini-Projects/Inventory-System/main.go
+++ Maps/Mini-Projects/Inventory-System/main.go
@@ -12,6 +12,7 @@ func main() {
 	//first, initialize inventory's method
 	ourStruct := ourStruct()
 
+retrry:
 	var userChoice int
 
 	fmt.Println("1. Stock Up")
@@ -19,11 +20,19 @@ func main() {
 
 	fmt.Scan(&userChoice)
 
-	//add item to the inventory... passing inventory's func as argument
-	fmt.Println("Stock Up! Add Item")
-	AddItem(ourStruct)
+	switch userChoice {
+	case 1:
+		//add item to the inventory... passing inventory's func as argument
+		fmt.Println("Stock Up! Add Item")
+		AddItem(ourStruct)
 
-	//check/display available items in the inventory...taking inventory's func as argument
-	fmt.Println("Check ")
-	Items(ourStruct)
+	case 2:
+		//check/display available items in the inventory...taking inventory's func as argument
+		fmt.Println("Check ")
+		Items(ourStruct)
+	default:
+		fmt.Println("Invalid Choice... Retry")
+		goto retrry
+	}
+
 }
