e286ac658ced5be52c23c29ab4a096169faf3631
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-15 17:19:07 +0300

    Modify main.go file

--- Maps/Mini-Projects/Inventory-System/main.go
+++ Maps/Mini-Projects/Inventory-System/main.go
@@ -1,11 +1,14 @@
 package main
 
+import "fmt"
+
 //entry point of the Application
 func main() {
 	//first, initialize inventory's method
 	ourStruct := ourStruct()
 
 	//add item to the inventory... passing inventory's func as argument
+	fmt.Println("Stock Up! Add Item")
 	AddItem(ourStruct)
 
 	//check/display available items in the inventory...taking inventory's func as argument
