d9a4d4fa5e8caed7596cc7c11d31af8b5d67c46e
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-15 22:47:23 +0300

    Modify stockTotalPrice.go; define func to return totalStock

--- Maps/Mini-Projects/Inventory-System/stockTotalPrice.go
+++ Maps/Mini-Projects/Inventory-System/stockTotalPrice.go
@@ -1,2 +1,28 @@
 // this program file gives worth of all items in the inventory
 package main
+
+import "fmt"
+
+//func takes return value of inventory.go's method as a parameter
+func inventoryTotal(inventory map[string]map[int]int) int {
+	//first initialize the parameter into a local variable
+	ourItems := inventory
+
+	totalStock := 0
+
+	//first check if the inventory [ourItems] is empty
+	if len(ourItems) == 0 {
+		fmt.Println("Ooops...! Inventory empty")
+	} else {
+		//range through ourItems[the inventory] and sum-up the prices of all items as totalStock
+
+		//price of items is inner map's key
+		for _, value := range ourItems {
+			for k := range value {
+				totalStock += k
+			}
+		}
+	}
+
+	return totalStock
+}
