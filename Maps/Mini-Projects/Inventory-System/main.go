774fd659e295831f9b3b8aaa443685045fe8e4b4
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-15 17:09:25 +0300

    Modify main.go file; call all methods in main

--- Maps/Mini-Projects/Inventory-System/main.go
+++ Maps/Mini-Projects/Inventory-System/main.go
@@ -1,5 +1,13 @@
 package main
 
+//entry point of the Application
 func main() {
+	//first, initialize inventory's method
+	ourStruct := ourStruct()
 
+	//add item to the inventory... passing inventory's func as argument
+	AddItem(ourStruct)
+
+	//check/display available items in the inventory...taking inventory's func as argument
+	Items(ourStruct)
 }
