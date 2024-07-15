51027704b1ecbd6aa7ab0a887f4a74553417218c
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-15 16:09:20 +0300

    Modify mapStruct; define its method

--- Maps/Mini-Projects/Inventory-System/mapStruct.go
+++ Maps/Mini-Projects/Inventory-System/mapStruct.go
@@ -1,1 +1,13 @@
-//this file creates our simple data struct [map for now]
+// this file creates our simple data struct [map for now]
+package main
+
+//the function creates a map as our Data Structure for this project
+//it returns a map
+func ourStruct() map[string]map[int]int {
+	//creating nested map
+	//map with another map as its value
+
+	theStruct := make(map[string]map[int]int)
+
+	return theStruct
+}
