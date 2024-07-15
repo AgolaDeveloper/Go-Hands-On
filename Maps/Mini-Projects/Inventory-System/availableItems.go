6f3ae58d0b6d03d8f72e22db0d2babab4a9f2bee
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-15 17:00:38 +0300

    Modify availableItems.go file; define its method

--- Maps/Mini-Projects/Inventory-System/availableItems.go
+++ Maps/Mini-Projects/Inventory-System/availableItems.go
@@ -1,2 +1,25 @@
-// this program file gives access and display to every item available in the inventory
+// this program file gives access to every item available in the inventory
 package main
+
+import "fmt"
+
+//function displays whether there is an item in the inventory
+//takes inventory assa parameter
+func Items(inventory func() map[string]map[int]int) {
+	//intialize the inventory's method
+	ourItems := inventory()
+
+	//checks whether inventory is empty and displays its state
+
+	if len(ourItems) == 0 {
+		//if its len is zero[it's empty]
+		fmt.Println("Empty Inventory... Stock up!")
+	} else {
+		//...then range through the inventory and display all the items
+		fmt.Println()
+		for key, value := range ourItems {
+			fmt.Printf("\n Name: %v %v \n", key, value)
+		}
+	}
+
+}
