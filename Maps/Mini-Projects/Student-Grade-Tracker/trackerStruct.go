f0a9c846b734f67321e51afe5542e4f8cddf0615
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-18 09:45:02 +0300

    Define func that creates our Data structure... the struct that'll store Students' grades

--- Maps/Mini-Projects/Student-Grade-Tracker/trackerStruct.go
+++ Maps/Mini-Projects/Student-Grade-Tracker/trackerStruct.go
@@ -3,3 +3,14 @@
 //making it easier to perform different operations on the already organized data
 
 package main
+
+//defining func that creates our Data structure... the struct that'll store Students' grades
+//it returns a map of studentName as key, its value an innerMap of key subjects and value grades
+
+func studentGrades() map[string]map[string]int {
+	//make an empty, but not nil, map
+
+	studentGrades := make(map[string]map[string]int)
+
+	return studentGrades
+}
