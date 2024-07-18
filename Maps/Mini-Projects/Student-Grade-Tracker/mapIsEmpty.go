fa39e46896ee52210c1aca54304c7a530155e430
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-18 10:54:00 +0300

    Add func to mapIsEmpty file; that checks whether the map data structure is empty and return bool

--- Maps/Mini-Projects/Student-Grade-Tracker/mapIsEmpty.go
+++ Maps/Mini-Projects/Student-Grade-Tracker/mapIsEmpty.go
@@ -1,2 +1,20 @@
 // this program file helps with checking whetehr our Map-Struct [that holds the Students' Grades is empty]
 package main
+
+//func that checks whether the map data structure is empty
+//it takes our map data structure as a parameter and ...
+//...returns boolean value [TRUE or FALSE]
+func mapIsEmpty(studentGrade map[string]map[string]string) bool {
+	//first initialize the map parameter to a local map variable
+	studentGradeStruct := studentGrade
+
+	//check whether empty
+
+	if len(studentGradeStruct) == 0 {
+		//if it's empty return true (implying...it's true ourMap is empty)
+		return true
+	} else {
+		//else if map is not empty return false (implying... ourMap is not empty)
+		return false
+	}
+}
