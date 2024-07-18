7963c0e55c35e0eae64a97f68b2b1b0f4c8f7319
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-18 12:06:07 +0300

    Modify main.go file; call methods from other files

--- Maps/Mini-Projects/Student-Grade-Tracker/main.go
+++ Maps/Mini-Projects/Student-Grade-Tracker/main.go
@@ -1,2 +1,18 @@
 // this is the main entry program to our mini project
 package main
+
+//main entry point
+func main() {
+	//first initialize func of our Data Struct
+	ourStruct := studentGrades()
+
+	//addStudentGrade
+	addStudentGrades(ourStruct)
+
+	//viewStudentGrade
+	viewStudentGrade(ourStruct, mapIsEmpty(ourStruct))
+
+	//deleteStudentGrade
+	deleteStudentGrade(ourStruct, mapIsEmpty(ourStruct))
+
+}
