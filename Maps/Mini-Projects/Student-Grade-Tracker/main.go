2dfff35455a022236cddab481435747d6717e222
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-18 12:18:11 +0300

    Add switch control to the functionalities in main.go

--- Maps/Mini-Projects/Student-Grade-Tracker/main.go
+++ Maps/Mini-Projects/Student-Grade-Tracker/main.go
@@ -1,18 +1,37 @@
 // this is the main entry program to our mini project
 package main
 
+import "fmt"
+
 //main entry point
 func main() {
 	//first initialize func of our Data Struct
 	ourStruct := studentGrades()
 
-	//addStudentGrade
-	addStudentGrades(ourStruct)
+retry:
+	var userChoice int
 
-	//viewStudentGrade
-	viewStudentGrade(ourStruct, mapIsEmpty(ourStruct))
+	fmt.Println("CHOOSE A FUNCTIONALITY:")
+	fmt.Println("1. ADD STUDENT GRADES:")
+	fmt.Println(" 2. VIEW STUDENT GRADES:")
+	fmt.Println("3. DELETE STUDENT GRADES:")
+	//fmt.Println("CHOOSE A FUNCTIONALITY:")
 
-	//deleteStudentGrade
-	deleteStudentGrade(ourStruct, mapIsEmpty(ourStruct))
+	fmt.Scan(&userChoice)
+
+	switch userChoice {
+	case 1:
+		//addStudentGrade
+		addStudentGrades(ourStruct)
+	case 2:
+		//viewStudentGrade
+		viewStudentGrade(ourStruct, mapIsEmpty(ourStruct))
+	case 3:
+		//deleteStudentGrade
+		deleteStudentGrade(ourStruct, mapIsEmpty(ourStruct))
+	default:
+		fmt.Println("Invalid Choice: Retry>>>")
+		goto retry
+	}
 
 }
