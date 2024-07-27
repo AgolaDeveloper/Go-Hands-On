51c62fae09f552bb5ccc9f1623d0c24e590bf8a5
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-27 23:21:39 +0300

    Define func that holds student details (assa slice of student objects)

--- Structs/student-management-system/studentStruct.go
+++ Structs/student-management-system/studentStruct.go
@@ -1,2 +1,10 @@
 // package that helps with creating our Data Structure that'll store the student details
 package main
+
+func ouStruct() {
+	//create the struct that will hold the student details
+	//this stuct shall hold slice of object students (another struct)
+	type Students struct {
+		Students []Student
+	}
+}
