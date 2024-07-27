aeeefdae7112b009f45537a44645e92765801a3d
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-27 23:33:50 +0300

    Define func that models object student

--- Structs/student-management-system/models/student.go
+++ Structs/student-management-system/models/student.go
@@ -1,2 +1,16 @@
 // this is a file that models student objects
 package main
+
+func student() struct{} {
+	//create a student struct
+
+	type Student struct {
+		firstName       string
+		lastName        string
+		admissionNumber string
+		class           string
+		age             int
+		subjects        map[string]int
+	}
+	return Student
+}
