6c1d9c4fba2a00c61c1bb2cb41c4cbea0d271182
AgolaDeveloper <owinoagola00@gmail.com>
2024-07-28 23:31:55 +0300

    Define func that'll delete student

---
+++ Structs/student-management-system/deleteStudent.go
@@ -0,0 +1,12 @@
+// package that helps with deleting student details from the the student struct
+package main
+
+//takes 2 parameters
+//student's admin number and struct holding list of student objects
+func deleteStudent(admin string, ourStruct struct{}) {
+	//first checks if student
+
+	for index, value := range ourStruct.Students {
+
+	}
+}
